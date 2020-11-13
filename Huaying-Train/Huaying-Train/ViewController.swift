732d605896b9b8e8176176eecbff7608cd309500
lixuefeng <calchas@vip.qq.com>
2020-11-13 13:42:47 +0800

    update

---
+++ Huaying-Train/Huaying-Train/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Huaying-Train
+//
+//  Created by 李雪峰 on 2020/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
