3ca5b3bfb4e04d7224636e7b41126ca304023e5f
lixuefeng <calchas@vip.qq.com>
2020-11-13 13:46:21 +0800

    fixed bug about 10.3

--- Huaying-Train/Huaying-Train/ViewController.swift
+++ Huaying-Train/Huaying-Train/ViewController.swift
@@ -5,15 +5,23 @@
 //  Created by 李雪峰 on 2020/11/13.
 //
 
-import UIKit
+// import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var wk:WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        self.wk = WKWebView(frame: self.view.frame)
+        let theConfiguration = WKWebViewConfiguration()
+        theConfiguration.userContentController = WKUserContentController() // 通过js与webview内容交互配置
+        let frame = CGRect(x: 0,y : 0, width: self.view.frame.width, height:self.view.frame.height) //定位wk位置
+        wk = WKWebView(frame: frame, configuration: theConfiguration)
+        wk.allowsLinkPreview = true
+        self.wk.load(NSURLRequest(url:NSURL(string:"https://www.huaying.info/Train/Login/index/authcode/16d902b3-3bfc-47a8-8dc6-081fdc183ea2.html")! as URL) as URLRequest)
+        self.view.addSubview(self.wk)
     }
-
-
 }
-
